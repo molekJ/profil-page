1b402f4cbfaae5fe20a690139e706dcc2aa1c2fd
Jakub Molicki <molickikuba@gmail.com>
2022-01-24 15:16:04 +0100

    new project added - todo-list

--- src/data/ProjectsData.tsx
+++ src/data/ProjectsData.tsx
@@ -3,6 +3,7 @@ import sunsiad from "../images/sunsiad.jpg";
 import diet from "../images/diet.jpg";
 import inprogress from "../images/inprogress.jpg";
 import weather from "../images/weather.jpg";
+import todolist from "../images/todolist.jpg";
 
 import css from "../icons/css.svg";
 import html from "../icons/html.svg";
@@ -14,34 +15,33 @@ import jira from "../icons/jira.svg";
 
 export const ProjectsData = [
   {
-    title: "sunsiadek",
+    title: "ToDoList",
     description:
-      'Jest to projekt grupowy, kończący bootcamp. Aplikacja przeznaczona dla społeczności lokalnych, takich jak wieś, mała miejscowość czy też osiedle w większym mieście. Użytkownik może dodawać ogłosznia w zależności od kategorii jaką to ogłosznie reprezentuje. Główny cel aplikacji to polepszenie relacji sąsiedzkich, lepsze poznanie się, niesienie pomocy oraz dbanie o wspólne dobro małych społeczności. Przykład: w kategorii "pomoc" możemy zaoferować zrobienie zakupów bądź wyprowadzenie psa.',
-    lightBg: false,
-    img: sunsiad,
-    alt: "susniad application dashboard",
+      "Klasyk klasyków. Prosta aplikacja w której możemy dodawać planowane zadania. Wykorzystałem Local Storage, dzięki czemu nasze zadania automatycznie zapisują się w pamięci przeglądarki i wczytują po ponownym jej uruchomnieniu.",
+    lightBg: true,
+    img: todolist,
+    alt: "todolist application dashboard",
     leftStart: true,
-    githubLink: "https://github.com/infoshareacademy/jfddr3-sunsiadek-app",
-    deployment: "https://infoshareacademy.github.io/jfddr3-sunsiadek-app",
-    icons: [html, css, js, react, firebase, jira],
+    githubLink: "https://github.com/molekJ/to-do-list",
+    deployment: "https://molekj.github.io/to-do-list/",
+    icons: [html, css, js, react, ts],
   },
   {
-    title: "bakery",
-    description:
-      "Jest to prosta gierka, w której robimy i sprzedajemy ciastka. Było to jedno z zadań domowych bootcampu, do wykonania w Vanilla JS. Z uwagi na mnogość zagwozdek postanowiłem wrócić do tego i napisać jeszcze raz - tym razem w React oraz TypeScript (jest to mój pierwszy projekt z wykorzystaniem TS). Wygląd nie powala, ale nie o wygląd tu chodzi.",
-    lightBg: true,
-    img: bakery,
-    alt: "bakery game",
+    title: "weather",
+    description: "Aplikacja pogodowa, wykorzystująca API openweathermap.org.",
+    lightBg: false,
+    img: weather,
+    alt: "weather application dashboard",
     leftStart: false,
-    githubLink: "https://github.com/molekJ/bakery/tree/master/my-app",
-    deployment: "https://molekj.github.io/bakery/",
+    githubLink: "https://github.com/molekJ/Weather",
+    deployment: "https://molekj.github.io/weather/",
     icons: [html, css, js, react, ts],
   },
   {
     title: "diet",
     description:
       'Odtworzenie projektu dostarczonego w pliku .psd. Interakjce: prosty slider, animacje na ":hover", zaznaczanie/odznaczanie ikon. Dobry projekt do utrwalenia CSS, zwłaszcza CSS Grid i Flexbox',
-    lightBg: false,
+    lightBg: true,
     img: diet,
     alt: "diet application dashboard",
     leftStart: true,
@@ -50,14 +50,27 @@ export const ProjectsData = [
     icons: [html, css, js, react],
   },
   {
-    title: "weather",
-    description: "Aplikacja pogodowa, wykorzystująca API openweathermap.org.",
-    lightBg: true,
-    img: weather,
-    alt: "weather application dashboard",
+    title: "bakery",
+    description:
+      "Jest to prosta gierka, w której robimy i sprzedajemy ciastka. Było to jedno z zadań domowych bootcampu, do wykonania w Vanilla JS. Z uwagi na mnogość zagwozdek postanowiłem wrócić do tego i napisać jeszcze raz - tym razem w React oraz TypeScript (jest to mój pierwszy projekt z wykorzystaniem TS). Wygląd nie powala, ale nie o wygląd tu chodzi.",
+    lightBg: false,
+    img: bakery,
+    alt: "bakery game",
     leftStart: false,
-    githubLink: "https://github.com/molekJ/Weather",
-    deployment: "https://molekj.github.io/weather/",
+    githubLink: "https://github.com/molekJ/bakery/tree/master/my-app",
+    deployment: "https://molekj.github.io/bakery/",
     icons: [html, css, js, react, ts],
+  },
+  {
+    title: "sunsiadek",
+    description:
+      'Jest to projekt grupowy, kończący bootcamp. Aplikacja przeznaczona dla społeczności lokalnych, takich jak wieś, mała miejscowość czy też osiedle w większym mieście. Użytkownik może dodawać ogłosznia w zależności od kategorii jaką to ogłosznie reprezentuje. Główny cel aplikacji to polepszenie relacji sąsiedzkich, lepsze poznanie się, niesienie pomocy oraz dbanie o wspólne dobro małych społeczności. Przykład: w kategorii "pomoc" możemy zaoferować zrobienie zakupów bądź wyprowadzenie psa.',
+    lightBg: true,
+    img: sunsiad,
+    alt: "susniad application dashboard",
+    leftStart: true,
+    githubLink: "https://github.com/infoshareacademy/jfddr3-sunsiadek-app",
+    deployment: "https://infoshareacademy.github.io/jfddr3-sunsiadek-app",
+    icons: [html, css, js, react, firebase, jira],
   },
 ];
